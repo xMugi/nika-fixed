462fb584232a1fa3b94b2ceae95f128cb520006f
Mugi <54444757+xMugi@users.noreply.github.com>
2024-03-28 18:42:43 +0000

    Update Random.hpp

--- nika-fixed/Random.hpp
+++ nika-fixed/Random.hpp
@@ -29,9 +29,9 @@ public:
             static bool startSg = false;
             static float traversalProgressTmp = 0.0;
  
-            float worldtime = mem::Read<float>(m_localPlayer->base + OFFSET_TIME_BASE); // Current time
-            float traversalStartTime = mem::Read<float>(m_localPlayer->base + OFFSET_TRAVERSAL_START_TIME); // Time to start wall climbing
-            float traversalProgress = mem::Read<float>(m_localPlayer->base + OFFSET_TRAVERSAL_PROGRESS); // Wall climbing, if > 0.87 it is almost over.
+            float worldtime = mem::Read<float>(m_localPlayer->base + OFF_TIME_BASE); // Current time
+            float traversalStartTime = mem::Read<float>(m_localPlayer->base + OFF_TRAVERSAL_START_TIME); // Time to start wall climbing
+            float traversalProgress = mem::Read<float>(m_localPlayer->base + OFF_TRAVERSAL_PROGRESS); // Wall climbing, if > 0.87 it is almost over.
             auto HangOnWall = -(traversalStartTime - worldtime);
  
             if (HangOnWall > 0.1 && HangOnWall < 0.12)
@@ -48,7 +48,7 @@ public:
             {
                 //printf ("sg Press jump\n");
                 mem::Write<int>(OFF_REGION + OFF_IN_JUMP + 0x8, 5);
-                while (mem::Read<float>(m_localPlayer->base + OFFSET_TIME_BASE) - startjumpTime < 0.011);
+                while (mem::Read<float>(m_localPlayer->base + OFF_TIME_BASE) - startjumpTime < 0.011);
                 {
                     mem::Write<int>(OFF_REGION + OFF_IN_DUCK + 0x8, 6);
                     std::this_thread::sleep_for(std::chrono::milliseconds(30));
@@ -64,10 +64,10 @@ public:
         int wallJumpNow = 0;
  
         static float onWallTmp = 0;
-        float onWall = mem::Read<float>(m_localPlayer->base + OFFSET_WALL_RUN_START_TIME);
+        float onWall = mem::Read<float>(m_localPlayer->base + OFF_WALL_RUN_START_TIME);
         if (onWall > onWallTmp + 0.1) // 0.1
         {
-            if (mem::Read<int>(OFF_REGION + OFFSET_IN_FORWARD) == 0)
+            if (mem::Read<int>(OFF_REGION + OFF_IN_FORWARD) == 0)
             {
                 wallJumpNow = 1;
                 //printf("wall jump Press jump\n");
@@ -79,10 +79,10 @@ public:
         onWallTmp = onWall;
  
         static float onEdgeTmp = 0;
-        float onEdge = mem::Read<float>(m_localPlayer->base + OFFSET_TRAVERSAL_PROGRESS);
+        float onEdge = mem::Read<float>(m_localPlayer->base + OFF_TRAVERSAL_PROGRESS);
         if (onEdge > onEdgeTmp + 0.1) // 0.1
         {
-            if (mem::Read<int>(OFF_REGION + OFFSET_IN_FORWARD) == 0)
+            if (mem::Read<int>(OFF_REGION + OFF_IN_FORWARD) == 0)
             {
                 wallJumpNow = 2;
                 //printf("wall jump onEdge Press jump\n");
@@ -181,46 +181,46 @@ public:
         long wephandle = mem::Read<long>(m_localPlayer->base + OFF_WEAPON_HANDLE);
         wephandle &= 0xffff;
         long wep_entity = m_localPlayer->weaponEntity;
-        float curTime = mem::Read<float>(m_localPlayer->base + OFFSET_TIME_BASE);
+        float curTime = mem::Read<float>(m_localPlayer->base + OFF_TIME_BASE);
         float endTime = curTime +5.5;
         std::map<int, std::vector<int>> weaponSkinMap;
         //Light ammo weapons
-        weaponSkinMap[104] = { 6 };   //WEAPON_P2020 
-        weaponSkinMap[80] = { 6 };   //WEAPON_RE45 
-        weaponSkinMap[79] = { 11 };   //WEAPON_ALTERNATOR 
-        weaponSkinMap[103] = { 2 };   //WEAPON_R99  
-        weaponSkinMap[0] = { 10 };     //WEAPON_R301   
-        weaponSkinMap[105] = { 2 };    //WEAPON_SPITFIRE 
-        weaponSkinMap[88] = { 5 };    //WEAPON_G7 
+        weaponSkinMap[WEAPON_P2020] = { 6 };   //WEAPON_P2020 
+        weaponSkinMap[WEAPON_RE45] = { 6 };   //WEAPON_RE45 
+        weaponSkinMap[WEAPON_ALTERNATOR] = { 11 };   //WEAPON_ALTERNATOR 
+        weaponSkinMap[WEAPON_R99] = { 2 };   //WEAPON_R99  
+        weaponSkinMap[WEAPON_R301] = { 10 };     //WEAPON_R301   
+        weaponSkinMap[WEAPON_SPITFIRE] = { 2 };    //WEAPON_SPITFIRE 
+        weaponSkinMap[WEAPON_G7] = { 5 };    //WEAPON_G7 
         //Heavy ammo weapons
-        weaponSkinMap[111] = { 10};   // Car-SMG 
-        weaponSkinMap[20] = { 6 };    // Rampage 
-        weaponSkinMap[110] = { 9 };      //3030 
-        weaponSkinMap[89] = {10 };   //WEAPON_HEMLOCK  
-        weaponSkinMap[87] = { 8 };    //FlatLine  
+        weaponSkinMap[WEAPON_CAR] = { 10};   // Car-SMG 
+        weaponSkinMap[WEAPON_RAMPAGE] = { 6 };    // Rampage 
+        weaponSkinMap[WEAPON_3030] = { 9 };      //3030 
+        weaponSkinMap[WEAPON_HEMLOCK] = {10 };   //WEAPON_HEMLOCK  
+        weaponSkinMap[WEAPON_FLATLINE] = { 8 };    //FlatLine  
         //Energy ammo weapons
-        weaponSkinMap[112] = { 8 };    //WEAPON_NEMESIS  
-        weaponSkinMap[109] = { 9 };    //WEAPON_VOLT 
-        weaponSkinMap[106] = { 7 };    //WEAPON_TRIPLE_TAKE 
-        weaponSkinMap[92] = { 3 };    //WEAPON_LSTAR 
-        weaponSkinMap[83] = { 5 };    //WEAPON_DEVOTION 
-        weaponSkinMap[85] = { 8 };    //WEAPON_HAVOC 
+        weaponSkinMap[WEAPON_NEMESIS] = { 8 };    //WEAPON_NEMESIS  
+        weaponSkinMap[WEAPON_VOLT] = { 9 };    //WEAPON_VOLT 
+        weaponSkinMap[WEAPON_TRIPLE_TAKE] = { 7 };    //WEAPON_TRIPLE_TAKE 
+        weaponSkinMap[WEAPON_LSTAR] = { 3 };    //WEAPON_LSTAR 
+        weaponSkinMap[WEAPON_DEVOTION] = { 5 };    //WEAPON_DEVOTION 
+        weaponSkinMap[WEAPON_HAVOC] = { 8 };    //WEAPON_HAVOC 
         //Sniper ammo weapons
-        weaponSkinMap[1] = { 5 };    //WEAPON_SENTINEL 
-        weaponSkinMap[82] = { 8 };    //WEAPON_CHARGE_RIFLE 
-        weaponSkinMap[84] = { 7 };    //WEAPON_LONGBOW 
+        weaponSkinMap[WEAPON_SENTINEL] = { 5 };    //WEAPON_SENTINEL 
+        weaponSkinMap[WEAPON_CHARGE_RIFLE] = { 8 };    //WEAPON_CHARGE_RIFLE 
+        weaponSkinMap[WEAPON_LONGBOW] = { 7 };    //WEAPON_LONGBOW 
         //Shotgun ammo weapons
-        weaponSkinMap[95] = { 5 };    //WEAPON_MOZAMBIQUE 
-        weaponSkinMap[86] = { 8 };    //WEAPON_EVA8 
-        weaponSkinMap[102] = { 7 };    //WEAPON_PEACEKEEPER 
-        weaponSkinMap[94] = { 5 };    //WEAPON_MASTIFF 
+        weaponSkinMap[WEAPON_MOZAMBIQUE] = { 5 };    //WEAPON_MOZAMBIQUE 
+        weaponSkinMap[WEAPON_EVA8] = { 8 };    //WEAPON_EVA8 
+        weaponSkinMap[WEAPON_PEACEKEEPER] = { 7 };    //WEAPON_PEACEKEEPER 
+        weaponSkinMap[WEAPON_MASTIFF] = { 5 };    //WEAPON_MASTIFF 
         //Legendary ammo weapons
-        weaponSkinMap[108] = { 5 };    //WEAPON_WINGMAN 
-        weaponSkinMap[101] = { 7 };    //WEAPON_PROWLER
-        weaponSkinMap[2] = { 3 };    //WEAPON_BOCEK
-        weaponSkinMap[91] = { 6 };    //WEAPON_KRABER
-        weaponSkinMap[158] = { 3 };    //WEAPON_THROWING_KNIFE
-        weaponSkinMap[212] = { 2 };    //WEAPON_THERMITE_GRENADE 
+        weaponSkinMap[WEAPON_WINGMAN] = { 5 };    //WEAPON_WINGMAN 
+        weaponSkinMap[WEAPON_PROWLER] = { 7 };    //WEAPON_PROWLER
+        weaponSkinMap[WEAPON_BOCEK] = { 3 };    //WEAPON_BOCEK
+        weaponSkinMap[WEAPON_KRABER] = { 6 };    //WEAPON_KRABER
+        weaponSkinMap[WEAPON_THROWING_KNIFE] = { 3 };    //WEAPON_THROWING_KNIFE
+        weaponSkinMap[WEAPON_THERMITE_GRENADE] = { 2 };    //WEAPON_THERMITE_GRENADE 
 
         if (m_configLoader->FEATURE_SKINCHANGER_ON){
             int waponIndex = mem::Read<int>(wep_entity + OFF_WEAPON_INDEX);
@@ -229,7 +229,7 @@ public:
             //printf("Weapon: %s Activated Skin ID: %d \n", WeaponName(waponIndex).c_str(), skinID);  
             mem::Write<int>(m_localPlayer->base + OFF_SKIN, skinID+1);
             mem::Write<int>(wep_entity + OFF_SKIN, skinID);
-            curTime = mem::Read<float>(m_localPlayer->base + OFFSET_TIME_BASE);
+            curTime = mem::Read<float>(m_localPlayer->base + OFF_TIME_BASE);
         }                    
     }
 };
